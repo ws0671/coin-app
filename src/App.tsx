e2e152c2312c41f7b5b3e38152a3269dd7a7bb31
ws0671 <ws0671@naver.com>
2023-02-16 17:18:31 +0900

    5.10 React Query part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Router from "./Router";
 import { createGlobalStyle } from "styled-components";
-
+import { ReactQueryDevtools } from "react-query/devtools";
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
   html, body, div, span, applet, object, iframe,
@@ -64,6 +64,7 @@ function App() {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true}></ReactQueryDevtools>
     </>
   );
 }
